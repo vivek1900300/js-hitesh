320bee96141314d701c3598d2c051f7637fc2ca0
Vivek Kumar <167867477+vivek1900300@users.noreply.github.com>
2024-06-15 17:54:17 +0000

    Conversion of datatypes

---
+++ 01_basics/conversionoperation.js
@@ -0,0 +1,39 @@
+
+// let score = undefined
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 ; False => 0
+
+let isLoggedIn = "Vivek"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+// 1 => true; 0=> False;
+// " " => false
+// "Vivek" => True
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
