531a0367aae172caeb90558ec2095350826acebc
Vivek Kumar <167867477+vivek1900300@users.noreply.github.com>
2024-06-15 12:40:26 +0000

    variable01

---
+++ 01_basics/variable01.js
@@ -0,0 +1,19 @@
+const accountId = 1234443
+let accountEmail = "vivek@google.com"
+var accountPassowrd = "12345"
+accountcity = "Delhi"
+let accountState;
+
+ // accountId = 2 // not allowed
+
+ accountEmail = "vviek@gooo.com"
+ accountPassowrd = "9923"
+ accountcity = "noida"
+
+
+ /* prefer not use var
+    because of issue in block scope and functional scope
+    */
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassowrd,accountcity,accountState])
